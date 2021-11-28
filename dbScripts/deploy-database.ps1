d0423f308710ef7fdf057c57a70f700b81b398aa
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-28 17:10:39 +0000

    logging the connection string

--- dbScripts/deploy-database.ps1
+++ dbScripts/deploy-database.ps1
@@ -9,6 +9,8 @@ $scriptPath = "$PSScriptRoot/Scripts"
 
 $databaseConnectionString = 'Data Source=kvjk46t9.instances.spawn.cc,32182;User ID=sa;Password=BP27cMYWx6XKWHlN;database=WideWorldImporters;Connection Timeout=10;'
 
+Write-Output "The connection string is: $databaseConnectionString"
+
 Add-Type -Path 'C:\Program Files\DbUp\dbup-core.dll'
 Add-Type -Path 'C:\Program Files\DbUp\dbup-sqlserver.dll'
 
