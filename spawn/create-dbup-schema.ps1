5e0243bfa32a656a2db47e19f55f756d95b08663
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-28 15:54:13 +0000

    Forgot to create the actual psCredential obect

--- spawn/create-dbup-schema.ps1
+++ spawn/create-dbup-schema.ps1
@@ -1,10 +1,13 @@
 $sqlInstance = $OctopusParameters["Octopus.Action[Spawn new data container].Output.server"]
+
 $plaintextPassword = $OctopusParameters["Octopus.Action[Spawn new data container].Output.saPassword"]
-$encryptedPassword = $saPassword | ConvertTo-SecureString -AsPlainText -Force
+$encryptedPassword = $plaintextPassword | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sa", $encryptedPassword
+
 $query = "CREATE SCHEMA DbUp"
 
 Write-Output "Importing module dbatools"
 import-module dbatools
 
 Write-Output "Creating DbUp schema"
-invoke-dbaquery -SqlInstance $sqlInstance -Query $query 
+invoke-dbaquery -SqlInstance $sqlInstance -Query $query -SqlCredential $cred
