d30119cfafb2d32be37e207a53174d5e677e4a5b
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-28 16:48:25 +0000

    oops, need to add the databasename back in

--- dbScripts/deploy-database.ps1
+++ dbScripts/deploy-database.ps1
@@ -15,7 +15,7 @@ Add-Type -Path 'C:\Program Files\DbUp\dbup-core.dll'
 Add-Type -Path 'C:\Program Files\DbUp\dbup-sqlserver.dll'
 
 $dbUp = [DbUp.DeployChanges]::To
-$dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, "$databaseServer;Connection Timeout=10;")
+$dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, "$databaseServer;database=$databaseName;Connection Timeout=10;")
 $dbUp = [StandardExtensions]::WithScriptsFromFileSystem($dbUp, $scriptPath)
 $dbUp = [SqlServerExtensions]::JournalToSqlTable($dbUp, $dbUpSchema, $dbUpTable)
 $dbUp = [StandardExtensions]::LogToConsole($dbUp)
