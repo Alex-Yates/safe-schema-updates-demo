47e5bce3d3b1b5f14fb95f695f962bf2e74a846d
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-29 06:23:20 +0000

    known bug: fails if variable already exists

--- spawn/update-project-variables.ps1
+++ spawn/update-project-variables.ps1
@@ -1,3 +1,5 @@
+# KNOWN BUG: Fails if variable already exists!
+
 $connectionString = $OctopusParameters["Octopus.Action[Spawn new data container].Output.DataContainerConnectionString"]
 
 function Set-OctopusVariable {
