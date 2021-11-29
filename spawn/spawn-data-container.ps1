8f9e4b50aad308aee3e7c80d8c2982ab2063057f
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-29 09:20:05 +0000

    this works in ISE on the tentacle

--- spawn/spawn-data-container.ps1
+++ spawn/spawn-data-container.ps1
@@ -14,18 +14,19 @@ $WideWorldImportersConnString = $rawConnectionString -replace ("master;","WideWo
 $connectionString = $WideWorldImportersConnString + ";Connection Timeout=10;"
 
 $saPasswordRow = $info | Where-Object {$_ -like "connectionstring: *"}
-$saPasswordwithSpaces = $saPasswordRow -split "password: "
-$saPassword = $saPasswordwithSpaces.trim()
+$saPassword = $saPasswordRow -split "password: "
+$saPassword = $saPassword -replace '\s','' # removing spaces
 
 $hostRow = $info | Where-Object {$_ -like "host: *"}
-$sqlHostWithSpaces = $hostRow -split "host: "
-$sqlhost = $sqlHostWithSpaces.trim()
+$sqlHost = $hostRow -split "host: "
+$sqlHost = $sqlHost -replace '\s','' # removing spaces
 
 $portRow = $info | Where-Object {$_ -like "port: *"}
-$portWithSpaces = $portRow -split "port: "
-$port = $portWithSpaces.trim()
+$port = $portRow -split "port: "
+$port = $port -replace '\s','' # removing spaces
 
 $sqlInstance = "$sqlhost,$port"
+$sqlInstance = $sqlInstance -replace '\s','' # removing spaces
 
 Set-OctopusVariable -name "DataContainerConnectionString" -value $connectionString
 Set-OctopusVariable -name "saPassword" -value $saPassword 
