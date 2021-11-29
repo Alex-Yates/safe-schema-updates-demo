a1b7a4ab76c9a6b4d4e73de425211a19a6ab380c
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-29 12:07:26 +0000

    Avoiding passing the conn string as a param

---
+++ dbScripts/deploy-facebook.ps1
@@ -0,0 +1,24 @@
+# Config
+$scriptPath = "$PSScriptRoot/FacebookScripts"
+$dbConnectionString = $FacebookConnString
+$dbUpSchema = "DbUp"
+$dbUpTable = "Journal"
+
+# Just logging
+Write-Output "Database scripts directory: $scriptPath"
+Write-Output "connectionString: $dbConnectionString"
+
+# You need to download these to the tentacle in advance
+Write-Output "Loading DbUp DLLs"
+Add-Type -Path 'C:\Program Files\DbUp\dbup-core.dll'       # https://www.nuget.org/packages/dbup-core/
+Add-Type -Path 'C:\Program Files\DbUp\dbup-sqlserver.dll'  # https://www.nuget.org/packages/dbup-sqlserver/
+
+# Performing DB update
+Write-Output "Preparing DbUp migration"
+$dbUp = [DbUp.DeployChanges]::To
+$dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, $dbConnectionString)
+$dbUp = [StandardExtensions]::WithScriptsFromFileSystem($dbUp, $scriptPath)
+$dbUp = [SqlServerExtensions]::JournalToSqlTable($dbUp, $dbUpSchema, $dbUpTable)
+$dbUp = [StandardExtensions]::LogToConsole($dbUp)
+Write-Output "Performing DbUp migration"
+$upgradeResult = $dbUp.Build().PerformUpgrade()
