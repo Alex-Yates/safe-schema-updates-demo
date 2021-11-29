f879478050bf94dc187722ede7920d2856e20dd9
Alex Yates <alex.yates@dlmconsultants.com>
2021-11-29 12:58:05 +0000

    switching back to the WideWorldImporters image

--- spawn/spawn-data-container.ps1
+++ spawn/spawn-data-container.ps1
@@ -1,7 +1,7 @@
 $containerName = $OctopusParameters["Octopus.Project.Name"] + "-" + $OctopusParameters["Octopus.Environment.Name"] + "-" + $OctopusParameters["Octopus.RunbookRun.Id"]
 
 Write-output "SPAWN: Creating data-container"
-spawnctl create data-container --image mssql-empty --lifetime 1h --name $containerName | out-null
+spawnctl create data-container --image mssql-wideworldimporters --lifetime 1h --name $containerName | out-null
 
 Write-output "SPAWN: Getting data-container info"
 $info = spawnctl get data-container $containerName -o yaml
